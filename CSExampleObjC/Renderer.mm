83585a6c3c1c67d24bc7707cd2653e7cf621ea7a
Jinwoo Kim <kidjinwoo@me.com>
2023-12-01 01:35:10 +0900

    no message

--- CSExampleObjC/Renderer.mm
+++ CSExampleObjC/Renderer.mm
@@ -6,22 +6,37 @@
 //
 
 #import "Renderer.hpp"
+#import "ShaderTypes.hpp"
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+
+size_t alignedUniformsSize() {
+    return (sizeof(UniformsArray) + 0xFF) & -0x100;
+}
+
+const intptr_t maxBuffersInFlight = 3;
 
 __attribute__((objc_direct_members))
 @interface Renderer ()
+@property (retain, nonatomic) cp_layer_renderer_t layerRenderer;
+@property (retain, nonatomic) id<MTLDevice> device;
 @end
 
 @implementation Renderer
 
 - (instancetype)initWithLayerRenderer:(cp_layer_renderer_t)layerRenderer {
     if (self = [super init]) {
+        id<MTLDevice> device = cp_layer_renderer_get_device(layerRenderer);
+        id<MTLCommandQueue> commandQueue = [device newCommandQueue];
         
+        self.layerRenderer = layerRenderer;
     }
     
     return self;
 }
 
 - (void)dealloc {
+    [_layerRenderer release];
     [super dealloc];
 }
 
